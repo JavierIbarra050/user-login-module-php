f762931c30323e77bc804b242954cb533adf375d
JavierIbarra050 <ibarra.161804@e.unavarra.es>
2026-05-27 13:21:12 +0200

    [PASO 2] - Crear test getSession y modificados tests previos con Dummies

--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace UserLoginService\Tests\Application;
 
 use PHPUnit\Framework\TestCase;
+use UserLoginService\Application\SessionManager;
 use UserLoginService\Application\UserLoginService;
 use UserLoginService\Domain\User;
 
@@ -15,7 +16,8 @@ final class UserLoginServiceTest extends TestCase
      */
     public function givenLoginOfUserWhoIsNotLoggedInReturnsArrayWithUser()
     {
-        $userLoginService = new UserLoginService();
+        $sessionManagerDummy = $this->createMock(SessionManager::class);
+        $userLoginService = new UserLoginService($sessionManagerDummy);
 
         $user = new User("username");
         $userLoginService->manualLogin($user);
@@ -25,16 +27,31 @@ final class UserLoginServiceTest extends TestCase
 
     /**
      * @test
-     * @throws \Exception
+     * @throws /Exception
      */
     public function givenUserWhoIsAlreadyLoggedInReturnsErrorException()
     {
-        $userLoginService = new UserLoginService();
+        $sessionManagerDummy = $this->createMock(SessionManager::class);
+        $userLoginService = new UserLoginService($sessionManagerDummy);
 
         $user = new User("username");
         $userLoginService->manualLogin($user);
 
         $this->expectExceptionMessage("User already logged in");
         $userLoginService->manualLogin($user);
+    }
+
+    /**
+     * @test
+     */
+    public function externalSessionsWorksRight(){
+        $sessionManagerStub = $this->createMock(SessionManager::class);
+        $sessionManagerStub->method('getSessions')->willReturn(2);
+
+        $userLoginService = new UserLoginService($sessionManagerStub);
+        $result = $userLoginService->getExternalSessions();
+
+        $this->assertEquals(2, $result);
+
     }
 }
